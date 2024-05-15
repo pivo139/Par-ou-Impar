b8638d10cf38e049c4b90419a29742294496983f
Pedro Ivo Maldonado Roschel <134627957+pivo139@users.noreply.github.com>
2024-05-14 21:08:05 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) {
+  int numeroDigitado;
+  string op = "s";
+
+    while(op.Equals("s")){
+    Console.WriteLine("Digite um número: ");
+  numeroDigitado = int.Parse(Console.ReadLine());
+    
+    if (numeroDigitado % 2 == 0){
+      Console.WriteLine("número par!");
+    }
+    
+    else {
+      Console.WriteLine("número ímpar!");
+      }
+      
+    Console.WriteLine("Deseja fazer outra operação? (s/n): ");
+      op = Console.ReadLine();
+      }
+    }
+  }
